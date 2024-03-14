151ce1cdcdef0847f26143d80949294316dec582
= <ahsaanrafiq8@gmail.com>
2024-03-15 00:55:51 +0500

    Completed the Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import FetchUserData from './Components/FetchUserData';
+import DisplayUserInfo from './Components/DisplayUserInfo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <FetchUserData/>
-      </header>
+        <DisplayUserInfo UserID={3}/>
     </div>
   );
 }
